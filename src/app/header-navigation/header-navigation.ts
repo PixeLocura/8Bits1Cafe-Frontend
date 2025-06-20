5c1024002819bae63e2c810e600ec0eff59f907e
u202210494 <u202210494@upc.edu.pe>
2025-06-20 00:03:29 -0500

    Add header navigation component with HTML, CSS, and TypeScript files; update .gitignore to exclude .github directory

---
+++ src/app/header-navigation/header-navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-navigation',
+  imports: [],
+  templateUrl: './header-navigation.html',
+  styleUrl: './header-navigation.css'
+})
+export class HeaderNavigation {
+
+}
