a5624f006588be1f38ad1c2cc980cdf38c11112f
Daniella Vargas <idaniellavargas@gmail.com>
2025-06-21 21:09:39 -0500

    feat: add not found page and improve navbaw with animation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register-component';
 
-export const routes: Routes = [];
-
-
 export const appRoutes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/register', pathMatch: 'full' },
+
+  // Página 404 - Ruta comodín
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then((m) => m.NotFoundComponent),
+  },
 ];
