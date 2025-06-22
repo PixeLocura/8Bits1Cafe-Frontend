2ccf611066ce77d4d61854d727a73967c1333511
Daniella Vargas <idaniellavargas@gmail.com>
2025-06-21 21:58:59 -0500

    feat: merge with footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderNavigation } from './header-navigation/header-navigation';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderNavigation],
+  imports: [RouterOutlet, HeaderNavigation, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
