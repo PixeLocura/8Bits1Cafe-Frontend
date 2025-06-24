0157bbde691d4244cad963f0b4447bb5ee09ccdf
Daniella Vargas <idaniellavargas@gmail.com>
2025-06-24 00:46:24 -0500

    feat: connect backend with register and login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = `${environment.backendEndpoint}/auth`;
+  private apiUrl = `https://eightbits.onrender.com/api/v1/auth`;
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSubject.asObservable();
 
@@ -19,6 +19,8 @@ export class AuthService {
     if (token && user) {
       this.currentUserSubject.next(JSON.parse(user));
     }
+
+    
   }
 
   login(credentials: LoginRequest): Observable<LoginResponse> {
@@ -63,4 +65,16 @@ export class AuthService {
   getCurrentUser(): User | null {
     return this.currentUserSubject.value;
   }
+
+  register(userData: {
+    name: string;
+    lastname: string;
+    username: string;
+    email: string;
+    password: string;
+    countryIso: string;
+  }): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/register/admin`, userData);
+  }
+  
 }
