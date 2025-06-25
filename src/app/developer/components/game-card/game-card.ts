f3b755b76bf68d6cbdb9ef6ec98842bcb945f26a
u202210494 <u202210494@upc.edu.pe>
2025-06-24 20:56:19 -0500

    feat: integrate router module into game card component for improved navigation

--- src/app/developer/components/game-card/game-card.ts
+++ src/app/developer/components/game-card/game-card.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Game } from '../../interfaces/developer.interfaces';
 import { CategoryInfo, LanguageInfo, PlatformInfo, Category, Language, Platform } from '../../../shared/interfaces/game.interfaces';
 
 @Component({
   selector: 'app-game-card',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe, DatePipe],
+  imports: [CommonModule, CurrencyPipe, DatePipe, RouterModule],
   templateUrl: './game-card.html',
   styleUrl: './game-card.css'
 })
