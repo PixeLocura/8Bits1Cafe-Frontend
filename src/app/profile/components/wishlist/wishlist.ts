721a72882496c1e9d61a3c7b8ab07ae7869e7005
sebasvp2005 <sebasvp2005@gmail.com>
2025-06-28 21:23:17 -0500

    fix: games

--- src/app/profile/components/wishlist/wishlist.ts
+++ src/app/profile/components/wishlist/wishlist.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GameCard} from '../game-card/game-card';
 import {NgForOf, NgIf} from '@angular/common';
 import {Game} from '../../../shared/models/game.model';
 import {MOCK_GAMES} from '../../../shared/mock/mock-games';
+import {HomeService} from '../../../home/services/home-service';
 
 @Component({
   selector: 'app-wishlist',
@@ -14,6 +15,18 @@ import {MOCK_GAMES} from '../../../shared/mock/mock-games';
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.css'
 })
-export class Wishlist {
-  wishlist: Game[] = MOCK_GAMES.slice(-2, -1);
+export class Wishlist implements OnInit{
+  wishlist: any  = null;
+
+  constructor(private homeService: HomeService) {
+
+  }
+
+  ngOnInit() {
+    this.homeService.getNewReleases().subscribe(u=>{
+      this.wishlist = u.slice(-2, -1)
+    })
+  }
+
+
 }
