28f058af41ab10bbb8cbfa4074218145006ba390
Crysti <103683122+crysticore@users.noreply.github.com>
2025-07-10 17:25:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   backendEndpoint: 'https://eightbits.onrender.com/api/v1'
   //backendEndpoint: 'http://localhost:8080/api/v1'
 };
